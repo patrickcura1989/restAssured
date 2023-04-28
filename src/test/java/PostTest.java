08efc5f5887c10867380d256562416b516e3b9de
LEGION-PC <patrick_cura1989@yahoo.com>
2023-04-28 14:49:34 +1000

    Apply gson to serialize classes and use objects in rest assured

---
+++ src/test/java/PostTest.java
@@ -0,0 +1,29 @@
+import Objects.User;
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.apache.http.HttpStatus;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+// https://www.eliasnogueira.com/the-best-way-to-add-a-request-body-to-a-post-request-using-rest-assured/
+class PostTest {
+
+    @Test
+    void postUsingObjectMapping() {
+
+        User user = new User("Elias", "Principal Engineer");
+
+        Response r = given().
+                contentType(ContentType.JSON).
+                body(user).
+                when().
+                post(" https://reqres.in/api/users");
+        r.then().statusCode(HttpStatus.SC_CREATED);
+
+        r.body().prettyPrint();
+
+        JsonPath bdy = r.jsonPath();
+        System.out.println(bdy.get("name").toString());
+    }
+}
